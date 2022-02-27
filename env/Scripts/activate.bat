320fbd022946a60bbc1ea3928aa640843c2cbf37
S.M.Samrat <freefire808709@gmail.com>
2022-02-28 00:02:56 +0600

    Modified some element

--- env/Scripts/activate.bat
+++ env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Administrator\Desktop\New folder (2)\env
+set VIRTUAL_ENV=C:\Users\DCL\Desktop\multiple_field_search_django\env
 
 if not defined PROMPT set PROMPT=$P$G
 
